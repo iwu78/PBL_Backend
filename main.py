e7849c4aeb31b7259161787561da1065a5e1a106
Shubhay <142445482+vynx1@users.noreply.github.com>
2024-03-29 10:25:55 -0700

    Edited Main.Py

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ from api.user import user_api # Blueprint import api definition
 from api.player import player_api
 from api.titanic import titanic_api
 from api.concussion import concussion_api
+from api.songs import Song_api
 # database migrations
 from model.users import initUsers
 from model.players import initPlayers
 from model.titanicML import initTitanic
 from model.concussion import initConcussion
+from model.songs import initSongs
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
@@ -37,6 +39,8 @@ app.register_blueprint(player_api)
 app.register_blueprint(titanic_api) # register api routes
 app.register_blueprint(app_projects) # register app pages
 app.register_blueprint(concussion_api)
+app.register_blueprint(Song_api)
+
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -61,6 +65,10 @@ def generate_data():
     initPlayers()
     initTitanic()
     initConcussion()
+
+@app.before_request
+def before_request():
+    initSongs()
 
 # Register the custom command group with the Flask application
 app.cli.add_command(custom_cli)
