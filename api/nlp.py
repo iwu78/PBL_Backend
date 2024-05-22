f51bcb5c5825bc8bd17e1d4e20bd3405e1e2526a
Shubhay <142445482+vynx1@users.noreply.github.com>
2024-05-22 11:17:39 -0700

    Model + main.py

--- api/nlp.py
+++ api/nlp.py
@@ -1,256 +1,103 @@
-import numpy as np
-import pandas as pd
-import os
+from flask import Blueprint, request, jsonify
+from flask_restful import Api, Resource
+import sqlite3
+import base64
+from PIL import Image
+from io import BytesIO
+import urllib.request
 import tensorflow as tf
-from tqdm import tqdm
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.utils import Sequence
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D, Activation, Dropout, Flatten, Dense, Input, Layer
-from tensorflow.keras.layers import Embedding, LSTM, add, Concatenate, Reshape, concatenate, Bidirectional
-from tensorflow.keras.applications import VGG16, ResNet50, DenseNet201
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.utils import plot_model
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-import warnings
-import matplotlib.pyplot as plt
-import seaborn as sns
-from textwrap import wrap
+import numpy as np
+import pickle
 
-plt.rcParams['font.size'] = 12
-sns.set_style("dark")
-warnings.filterwarnings('ignore')
+caption_api = Blueprint('caption_api', __name__, url_prefix='/api/caption')
+api = Api(caption_api)
 
-image_path = '../input/flickr8k/Images'
+class ImageCaptioningModel:
+    def __init__(self, model_path, tokenizer_path, max_length_path, fe_model):
+        self.caption_model = tf.keras.models.load_model(model_path)
+        with open(tokenizer_path, 'rb') as handle:
+            self.tokenizer = pickle.load(handle)
+        with open(max_length_path, 'r') as f:
+            self.max_length = int(f.read())
+        self.fe_model = fe_model
 
-data = pd.read_csv("../input/flickr8k/captions.txt")
-data.head()
+    def read_image(self, path, img_size=224):
+        img = load_img(path, color_mode='rgb', target_size=(img_size, img_size))
+        img = img_to_array(img)
+        img = img / 255.0
+        return img
 
-def readImage(path,img_size=224):
-    img = load_img(path,color_mode='rgb',target_size=(img_size,img_size))
-    img = img_to_array(img)
-    img = img/255.
-    
-    return img
+    def extract_features(self, image_path):
+        img = load_img(image_path, target_size=(224, 224))
+        img = img_to_array(img)
+        img = img / 255.0
+        img = np.expand_dims(img, axis=0)
+        feature = self.fe_model.predict(img, verbose=0)
+        return feature
 
-def display_images(temp_df):
-    temp_df = temp_df.reset_index(drop=True)
-    plt.figure(figsize = (20 , 20))
-    n = 0
-    for i in range(15):
-        n+=1
-        plt.subplot(5 , 5, n)
-        plt.subplots_adjust(hspace = 0.7, wspace = 0.3)
-        image = readImage(f"../input/flickr8k/Images/{temp_df.image[i]}")
-        plt.imshow(image)
-        plt.title("\n".join(wrap(temp_df.caption[i], 20)))
-        plt.axis("off")
+    def idx_to_word(self, integer):
+        for word, index in self.tokenizer.word_index.items():
+            if index == integer:
+                return word
+        return None
 
-display_images(data.sample(15))
-
-def text_preprocessing(data):
-    data['caption'] = data['caption'].apply(lambda x: x.lower())
-    data['caption'] = data['caption'].apply(lambda x: x.replace("[^A-Za-z]",""))
-    data['caption'] = data['caption'].apply(lambda x: x.replace("\s+"," "))
-    data['caption'] = data['caption'].apply(lambda x: " ".join([word for word in x.split() if len(word)>1]))
-    data['caption'] = "startseq "+data['caption']+" endseq"
-    return data
-
-data = text_preprocessing(data)
-captions = data['caption'].tolist()
-captions[:10]
+    def predict_caption(self, image_path):
+        feature = self.extract_features(image_path)
+        in_text = "startseq"
+        for i in range(self.max_length):
+            sequence = self.tokenizer.texts_to_sequences([in_text])[0]
+            sequence = pad_sequences([sequence], maxlen=self.max_length)
+            y_pred = self.caption_model.predict([feature, sequence], verbose=0)
+            y_pred = np.argmax(y_pred)
+            word = self.idx_to_word(y_pred)
+            if word is None:
+                break
+            in_text += " " + word
+            if word == 'endseq':
+                break
+        return in_text
 
 
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(captions)
-vocab_size = len(tokenizer.word_index) + 1
-max_length = max(len(caption.split()) for caption in captions)
+# Initialize the model
+fe = tf.keras.applications.DenseNet201(weights='imagenet', include_top=False, pooling='avg')
+caption_model = ImageCaptioningModel('model.h5', 'tokenizer.pkl', 'max_length.txt', fe)
 
-images = data['image'].unique().tolist()
-nimages = len(images)
+class ImageApi:
+    class Upload(Resource):
+        def post(self):
+            try:
+                data = request.get_json()
+                image_data = data.get('image')
 
-split_index = round(0.85*nimages)
-train_images = images[:split_index]
-val_images = images[split_index:]
+                conn = sqlite3.connect('sqlite.db')
+                cursor = conn.cursor()
 
-train = data[data['image'].isin(train_images)]
-test = data[data['image'].isin(val_images)]
+                cursor.execute('''CREATE TABLE IF NOT EXISTS images (id INTEGER PRIMARY KEY AUTOINCREMENT, image_data BLOB)''')
+                cursor.execute("INSERT INTO images (image_data) VALUES (?)", (image_data,))
+                conn.commit()
+                conn.close()
 
-train.reset_index(inplace=True,drop=True)
-test.reset_index(inplace=True,drop=True)
+                return jsonify({'message': 'Image uploaded successfully'})
+            except Exception as e:
+                return jsonify({'error': str(e)}), 500
 
-tokenizer.texts_to_sequences([captions[1]])[0]
+    class Predict(Resource):
+        def post(self):
+            try:
+                data = request.get_json()
+                url = data["image_data"]
+                with urllib.request.urlopen(url) as response:
+                    with open('temp.jpg', 'wb') as f:
+                        f.write(response.read())
 
+                image_path = 'temp.jpg'
+                caption = caption_model.predict_caption(image_path)
 
-model = DenseNet201()
-fe = Model(inputs=model.input, outputs=model.layers[-2].output)
+                return jsonify({'caption': caption})
+            except Exception as e:
+                return jsonify({'error': str(e)}), 500
 
-img_size = 224
-features = {}
-for image in tqdm(data['image'].unique().tolist()):
-    img = load_img(os.path.join(image_path,image),target_size=(img_size,img_size))
-    img = img_to_array(img)
-    img = img/255.
-    img = np.expand_dims(img,axis=0)
-    feature = fe.predict(img, verbose=0)
-    features[image] = feature
+# Add resources to API
+api.add_resource(ImageApi.Upload, '/upload')
+api.add_resource(ImageApi.Predict, '/predict')
 
-
-class CustomDataGenerator(Sequence):
-    
-    def __init__(self, df, X_col, y_col, batch_size, directory, tokenizer, 
-                 vocab_size, max_length, features,shuffle=True):
-    
-        self.df = df.copy()
-        self.X_col = X_col
-        self.y_col = y_col
-        self.directory = directory
-        self.batch_size = batch_size
-        self.tokenizer = tokenizer
-        self.vocab_size = vocab_size
-        self.max_length = max_length
-        self.features = features
-        self.shuffle = shuffle
-        self.n = len(self.df)
-        
-    def on_epoch_end(self):
-        if self.shuffle:
-            self.df = self.df.sample(frac=1).reset_index(drop=True)
-    
-    def __len__(self):
-        return self.n // self.batch_size
-    
-    def __getitem__(self,index):
-    
-        batch = self.df.iloc[index * self.batch_size:(index + 1) * self.batch_size,:]
-        X1, X2, y = self.__get_data(batch)        
-        return (X1, X2), y
-    
-    def __get_data(self,batch):
-        
-        X1, X2, y = list(), list(), list()
-        
-        images = batch[self.X_col].tolist()
-           
-        for image in images:
-            feature = self.features[image][0]
-            
-            captions = batch.loc[batch[self.X_col]==image, self.y_col].tolist()
-            for caption in captions:
-                seq = self.tokenizer.texts_to_sequences([caption])[0]
-
-                for i in range(1,len(seq)):
-                    in_seq, out_seq = seq[:i], seq[i]
-                    in_seq = pad_sequences([in_seq], maxlen=self.max_length)[0]
-                    out_seq = to_categorical([out_seq], num_classes=self.vocab_size)[0]
-                    X1.append(feature)
-                    X2.append(in_seq)
-                    y.append(out_seq)
-            
-        X1, X2, y = np.array(X1), np.array(X2), np.array(y)
-                
-        return X1, X2, y
-    
-
-input1 = Input(shape=(1920,))
-input2 = Input(shape=(max_length,))
-
-img_features = Dense(256, activation='relu')(input1)
-img_features_reshaped = Reshape((1, 256), input_shape=(256,))(img_features)
-
-sentence_features = Embedding(vocab_size, 256, mask_zero=False)(input2)
-merged = concatenate([img_features_reshaped,sentence_features],axis=1)
-sentence_features = LSTM(256)(merged)
-x = Dropout(0.5)(sentence_features)
-x = add([x, img_features])
-x = Dense(128, activation='relu')(x)
-x = Dropout(0.5)(x)
-output = Dense(vocab_size, activation='softmax')(x)
-
-caption_model = Model(inputs=[input1,input2], outputs=output)
-caption_model.compile(loss='categorical_crossentropy',optimizer='adam')
-
-plot_model(caption_model)
-
-caption_model.summary()
-
-train_generator = CustomDataGenerator(df=train,X_col='image',y_col='caption',batch_size=64,directory=image_path,
-                                      tokenizer=tokenizer,vocab_size=vocab_size,max_length=max_length,features=features)
-
-validation_generator = CustomDataGenerator(df=test,X_col='image',y_col='caption',batch_size=64,directory=image_path,
-                                      tokenizer=tokenizer,vocab_size=vocab_size,max_length=max_length,features=features)
-
-model_name = "model.h5"
-checkpoint = ModelCheckpoint(model_name,
-                            monitor="val_loss",
-                            mode="min",
-                            save_best_only = True,
-                            verbose=1)
-
-earlystopping = EarlyStopping(monitor='val_loss',min_delta = 0, patience = 5, verbose = 1, restore_best_weights=True)
-
-learning_rate_reduction = ReduceLROnPlateau(monitor='val_loss', 
-                                            patience=3, 
-                                            verbose=1, 
-                                            factor=0.2, 
-                                            min_lr=0.00000001)
-history = caption_model.fit(
-        train_generator,
-        epochs=50,
-        validation_data=validation_generator,
-        callbacks=[checkpoint,earlystopping,learning_rate_reduction])
-plt.figure(figsize=(20,8))
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.show()
-
-def idx_to_word(integer,tokenizer):
-    
-    for word, index in tokenizer.word_index.items():
-        if index==integer:
-            return word
-    return None
-
-def predict_caption(model, image, tokenizer, max_length, features):
-    
-    feature = features[image]
-    in_text = "startseq"
-    for i in range(max_length):
-        sequence = tokenizer.texts_to_sequences([in_text])[0]
-        sequence = pad_sequences([sequence], max_length)
-
-        y_pred = model.predict([feature,sequence])
-        y_pred = np.argmax(y_pred)
-        
-        word = idx_to_word(y_pred, tokenizer)
-        
-        if word is None:
-            break
-            
-        in_text+= " " + word
-        
-        if word == 'endseq':
-            break
-            
-    return in_text 
-
-samples = test.sample(15)
-samples.reset_index(drop=True,inplace=True)
-
-for index,record in samples.iterrows():
-
-    img = load_img(os.path.join(image_path,record['image']),target_size=(224,224))
-    img = img_to_array(img)
-    img = img/255.
-    
-    caption = predict_caption(caption_model, record['image'], tokenizer, max_length, features)
-    samples.loc[index,'caption'] = caption
-
-display_images(samples)
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import threading
 
 # import "packages" from flask
-from flask import render_template,request  # import render_template from "public" flask libraries
+from flask import render_template, request  # import render_template from "public" flask libraries
 from flask.cli import AppGroup
-
 
 # import "packages" from "this" project
 from __init__ import app, db, cors  # Definitions initialization
 
-
 # setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
+from api.covid import covid_api  # Blueprint import api definition
+from api.joke import joke_api  # Blueprint import api definition
+from api.user import user_api  # Blueprint import api definition
 from api.player import player_api
 from api.titanic import titanic_api
 from api.collaborapost import post_api
@@ -20,7 +18,9 @@ from api.concussion import concussion_api
 from api.songs import Song_api
 from api.model import model_api
 from api.image import images_bp
-# from api.stockMLapi import stock_api
+from api.nlp import nlp_api
+from api.caption import caption_api  # Import the new caption API
+
 # database migrations
 from model.users import initUsers
 from model.players import initPlayers
@@ -30,26 +30,25 @@ from model.songs import initSongs
 from model.images import initEasyImages
 
 # setup App pages
-from projects.projects import app_projects # Blueprint directory import projects definition
-
+from projects.projects import app_projects  # Blueprint directory import projects definition
 
 # Initialize the SQLAlchemy object to work with the Flask app instance
 db.init_app(app)
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
+app.register_blueprint(joke_api)  # register api routes
+app.register_blueprint(covid_api)  # register api routes
+app.register_blueprint(user_api)  # register api routes
 app.register_blueprint(player_api)
-app.register_blueprint(titanic_api) # register api routes
-app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(titanic_api)  # register api routes
+app.register_blueprint(app_projects)  # register app pages
 app.register_blueprint(post_api)
 app.register_blueprint(concussion_api)
 app.register_blueprint(Song_api)
 app.register_blueprint(model_api)
 app.register_blueprint(images_bp)
-# app.register_blueprint(stock_api)
-
+app.register_blueprint(nlp_api)
+app.register_blueprint(caption_api)  # register the new caption API
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -76,16 +75,16 @@ def generate_data():
     initConcussion()
     initEasyImages()
 
-
 @app.before_request
 def before_request():
     initSongs()
     allowed_origin = request.headers.get('Origin')
     if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://nighthawkcoders.github.io']:
         cors._origins = allowed_origin
+
 # Register the custom command group with the Flask application
 app.cli.add_command(custom_cli)
-        
+
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
